caf418bf65d12b8937193109b8d2632a6d1a6298
George Darling <george.darling@decisionsource.com>
2016-10-13 23:27:33 -0500

    Moved print() to global scope

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+function print(stringToPrint){
+	console.log(stringToPrint);
+	document.body.innerHTML += stringToPrint + '<br/>';
+}
+
 window.onload = function() {
 	console.log('hello world');
-	function print(stringToPrint){
-		console.log(stringToPrint);
-		document.body.innerHTML += stringToPrint + '<br/>';
-	}
 	print('test');
 };
