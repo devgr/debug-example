87536da46e380deb19cb16bfcdaec71a791b07cc
George Darling <george.darling@decisionsource.com>
2016-10-13 23:52:57 -0500

    added a subtract function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 console.log('hello world');
 var number = simpleMath.add(2, 2);
 console.log(number);
+number = simpleMath.subtract(5, 3);
+console.log(number);
--- simple-math.js
+++ simple-math.js
@@ -1,5 +1,8 @@
 var simpleMath = {
 	add: function(a, b){
 		return a + b;
+	},
+	subtract: function(a, b){
+		return a - b;
 	}
 };
