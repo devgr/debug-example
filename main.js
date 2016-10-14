e20ee4ae91d587eb308b8e9d011861bce0e81444
George Darling <george.darling@decisionsource.com>
2016-10-13 23:24:46 -0500

    fixed printing so that it doesn't happen until after the page is loaded.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-console.log('hello world');
-function print(stringToPrint){
-	console.log(stringToPrint);
-	document.body.innerHtml += stringToPrint + '<br/>';
-}
-print('test');
+window.onload = function() {
+	console.log('hello world');
+	function print(stringToPrint){
+		console.log(stringToPrint);
+		document.body.innerHTML += stringToPrint + '<br/>';
+	}
+	print('test');
+};
